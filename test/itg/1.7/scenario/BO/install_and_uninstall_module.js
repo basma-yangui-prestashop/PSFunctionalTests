6e9974114449b5a086631cdebe344baf04c66f8f
Basma YANGUI <basma.yangui@prestashop.com>
2017-04-21 15:57:43 +0200

    Format 1.7 code

--- test/itg/1.7/scenario/BO/install_and_uninstall_module.js
+++ test/itg/1.7/scenario/BO/install_and_uninstall_module.js
@@ -10,30 +10,30 @@ var uninstall_red_validation_is_visible = false;
 var only_filename = __filename.slice(__dirname.length + 1, -3);
 
 
-describe('The Install of a Module and its Uninstall', function(){
+describe('The Install of a Module and its Uninstall', function () {
 
 
-	common.initMocha.call(this);
-	
-	before(function(done){
-		this.selector = globals.selector;
-		this.client.call(done);
-	});
+    common.initMocha.call(this);
 
-	process.on('uncaughtException', common.take_screenshot);
+    before(function (done) {
+        this.selector = globals.selector;
+        this.client.call(done);
+    });
+
+    process.on('uncaughtException', common.take_screenshot);
     process.on('ReferenceError', common.take_screenshot);
 
-	after(common.after);
+    after(common.after);
 
-	describe('Log in in Back Office', function(done){
-        it('should log in successfully in BO', function(done){
-            global.fctname= this.test.title;
-			this.client
-				.signinBO()
-				.isVisible(this.selector.exit_welcome).then(function(isVisible) {
-					exit_welcome = isVisible;
-				})
-				.waitForExist(this.selector.menu, 90000)
+    describe('Log in in Back Office', function (done) {
+        it('should log in successfully in BO', function (done) {
+            global.fctname = this.test.title;
+            this.client
+                .signinBO()
+                .isVisible(this.selector.exit_welcome).then(function (isVisible) {
+                exit_welcome = isVisible;
+            })
+                .waitForExist(this.selector.menu, 90000)
                 .call(done);
         });
     });
@@ -77,23 +77,23 @@ describe('The Install of a Module and its Uninstall', function(){
             global.fctname = this.test.title;
             if (red_validation_is_visible) {
                 this.client
-            	    .getText(this.selector.red_validation).then(function(text) {
-                        done(new Error(text));
-                    })
-            }else if (green_validation_is_visible){
-                done();
-            }else{
-                done();
+                    .waitForExist(this.selector.exit_welcome, 90000)
+                    .click(this.selector.exit_welcome);
             }
-	    });
-	});
-		
-	describe('Uninstall module', function(done){
-        it('should go to the module and click on uninstall button', function(done){
-            global.fctname= this.test.title;
-            if (red_validation_is_visible){
-		        done(new Error("Unavailable module"));
-		    }else{
+            this.client
+                .pause(5000)
+                .click(this.selector.modules_menu)
+                .waitForExist(this.selector.modules_page_loaded, 90000)
+                .call(done);
+        });
+    });
+
+    describe('Uninstall module', function (done) {
+        it('should go to the module and click on uninstall button', function (done) {
+            global.fctname = this.test.title;
+            if (red_validation_is_visible) {
+                done(new Error("Unavailable module"));
+            } else {
                 this.client
                     .click(this.selector.modules_installed)
                     .waitForExist(this.selector.modules_page_loaded, 90000)
@@ -148,16 +148,16 @@ describe('The Install of a Module and its Uninstall', function(){
                 }
 
             }
-		});
-	})
-		
-	describe('Log out in Back Office', function(done){
-        it('should log out successfully in BO', function(done){
-            global.fctname= this.test.title;
-			this.client
-				.signoutBO()
-				.call(done);
-		});
-	});
+        });
+    })
+
+    describe('Log out in Back Office', function (done) {
+        it('should log out successfully in BO', function (done) {
+            global.fctname = this.test.title;
+            this.client
+                .signoutBO()
+                .call(done);
+        });
+    });
 
 });
